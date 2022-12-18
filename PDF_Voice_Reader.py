afcbaadf8eccef476bcb95dfa2ed766d94ec2579
Sam Mohan <samfranklinm@gmail.com>
2022-12-18 10:46:52 +0530

    added infinite loop upon wrong entry instead of exiting the program right away

--- PDF_Voice_Reader.py
+++ PDF_Voice_Reader.py
@@ -17,28 +17,43 @@ textToSpeech = pyttsx3.init()
 # Let the user know how many pages there are in the PDF
 for i in range(0, pdfRead.numPages):
     numOfPages = i
-print('There are ', numOfPages, ' pages in this PDF.')
+print('There is/are', numOfPages + 1, 'page(s) in this PDF.')
 
-# Ask user if there is a specific page that they would like to listen to
-pageOrNot = input(
-    'Is there a specific page you want me to read? (yes or no):  ')
-cleanedUpText = ''
+while True:
+    # Ask user if there is a specific page that they would like to listen to
+    pageOrNot = input(
+        'Is there a specific page you want me to read? (yes or no):  ')
+    cleanedUpText = ''
 
-# If so, ask user for page number and then extract the test from the page
-if pageOrNot == 'yes':
-    pageNumber = int(input('Ok, which page? '))
-    extractedText = pdfRead.getPage(pageNumber).extract_text()
-    cleanedUpText = extractedText.strip().replace('/n', ' ')
-    print(cleanedUpText)
-# Otherwise, extract text from all pages
-else:
-    for pageNumber in range(pdfRead.numPages):
-        extractedText = pdfRead.getPage(pageNumber).extract_text()
-        cleanedUpText = extractedText.strip().replace('/n', ' ')
-        print(cleanedUpText)
+    # If so, ask user for page number and then extract the test from the page
+    if pageOrNot == 'yes':
+        pageNumber = int(
+            input('Ok, which page? (NOTE: Page numbers begin with 0) '))
+        if pageNumber <= numOfPages:
+            try:
+                extractedText = pdfRead.getPage(pageNumber).extract_text()
+                cleanedUpText = extractedText.strip().replace('/n', ' ')
+                print(cleanedUpText)
+            except:
+                print('Unable to extract text from this page/PDF.')
+                break
+        else:
+            print('Invalid page number. Try again.')
+            continue
+    # Otherwise, extract text from all pages
+    elif pageOrNot == 'no':
+        for pageNumber in range(pdfRead.numPages):
+            extractedText = pdfRead.getPage(pageNumber).extract_text()
+            cleanedUpText = extractedText.strip().replace('/n', ' ')
+            print(cleanedUpText)
+    else:
+        print('Invalid input. Please select either `yes` or `no`.')
+        continue
+    # Read the extracted text to the user
+    textToSpeech.say(cleanedUpText)
+    print('Reading...')
+    textToSpeech.runAndWait()
+    print('Completed.')
+    break
 
-# Read the extracted text to the user
-textToSpeech.say(cleanedUpText)
-print('Reading...')
-textToSpeech.runAndWait()
-print('Completed.')
+exit()
